d17a05d4fe9272137570fff4828828e35c1a35d1
vlad <start2top@gmail.com>
2021-05-22 04:51:52 +0300

    3 sections

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+var name = "Vlad";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+
+let leftBorderWidth = 200;
+
+
+// number 
+// string - "", '', ``
+// true/false
+// null
+// undefined
+// let obj {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200 
+
+// }
+// Symbol
+
+//alert(1234)
+//console.log(number);
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+
+// let answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+console.log(4+4);
