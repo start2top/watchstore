e54b584ef1246968554d37a1649cc24a48228cd5
vlad <start2top@gmail.com>
2021-05-24 12:57:35 +0300

    added carousel and tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,86 @@
-var name = "Vlad";
+  // $(document).ready(function(){
+  //     $('.carousel__inner').slick({
+  //         speed: 1200,
+  //       // adaptiveHeight: true,
+  //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+  //         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+  //         responsive: [
+  //             {
+  //               breakpoint: 1024,
+  //               settings: {
+  //                 slidesToShow: 3,
+  //                 slidesToScroll: 3,
+  //                 infinite: true,
+  //                 dots: false
+  //               }
+  //             },
+  //             {
+  //               breakpoint: 600,
+  //               settings: {
+  //                 slidesToShow: 3,
+  //                 slidesToScroll: 3
+  //               }
+  //             },
+  //             {
+  //               breakpoint: 480,
+  //               settings: {
+  //                 slidesToShow: 3,
+  //                 slidesToScroll: 3
+  //               }
+  //             }
+  //             // You can unslick at a given breakpoint now by adding:
+  //             // settings: "unslick"
+  //             // instead of a settings object
+  //           ]
+  //     });
+  //   });
 
-let number = 7;
-const pi = 3.14;
 
-number = 4;
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+      320:{
+        controls: false,
+        autoWidth: true
+      },
+      640: {
+        edgePadding: 20,
+        gutter: 20,
+        items: 1,
+        autoWidth: true
+      },
+      768: {
+        gutter: 30,
+        controls: false,
+        autoWidth: true
+      },
+      991: {
+        items: 1,
+        controls: false,
+        autoWidth: true
+      }
+    }
+ });
 
+ document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
 
-let leftBorderWidth = 200;
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
 
-
-// number 
-// string - "", '', ``
-// true/false
-// null
-// undefined
-// let obj {
-//     name: 'apple',
-//     color: 'green',
-//     weight: 200 
-
-// }
-// Symbol
-
-//alert(1234)
-//console.log(number);
-// let answer = confirm("Вам есть 18?");
-// console.log(answer);
-
-// let answer = prompt("Вам есть 18?", "");
-// console.log(answer);
-
-console.log(4+4);
+(function($) {
+  $(function() {
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+  });
